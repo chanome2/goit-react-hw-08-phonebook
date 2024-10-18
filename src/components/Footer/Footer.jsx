fbda1b13d00b04c20fab5f6f463fce40b281f7f2
chanome2 <montalesc@ymail.com>
2024-10-18 22:15:18 +0800

    tez

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,31 @@
-import { Box, Typography, BottomNavigation } from "@mui/material";
+import { Box, Typography, BottomNavigation } from '@mui/material';
 // import css from "./Footer.module.css";
 
 export const Footer = () => {
-    return (
-        <>
-            <BottomNavigation sx={{backgroundColor: "#62487bae"}}>
-            <Box sx={{position: "static"}}>
-                
-                <Typography sx={{ color: "white", justifyContent: "center", height: "50px", alignItems: "center", display: "flex"}}>&copy; {new Date().getFullYear()} |  Developed by &nbsp;<a style={{fontSize: "bold", color: "lightgreen"}} href="mayotopia04.github.io/goit-react-hw-08-phonebook">mayotopia04</a>&nbsp; of GoIT Global🧡
-                </Typography>
-                </Box>
-            </BottomNavigation>
-        </>
-    );
+  return (
+    <>
+      <BottomNavigation sx={{ backgroundColor: '#62487bae' }}>
+        <Box sx={{ position: 'static' }}>
+          <Typography
+            sx={{
+              color: 'white',
+              justifyContent: 'center',
+              height: '50px',
+              alignItems: 'center',
+              display: 'flex',
+            }}
+          >
+            &copy; {new Date().getFullYear()} | Developed by &nbsp;
+            <a
+              style={{ fontSize: 'bold', color: 'lightgreen' }}
+              href="chanome2.github.io/goit-react-hw-08-phonebook"
+            >
+              chanome2
+            </a>
+            &nbsp; of GoIT Global🧡
+          </Typography>
+        </Box>
+      </BottomNavigation>
+    </>
+  );
 };
